6e0343550eef5facfe29baefdb28fd8fa36ccb11
christophervenegas2 <christophervenegas2@gmail.com>
2020-12-22 12:44:00 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app_products/ui/listview_product.dart';
+
+void main() => runApp(new MaterialApp(
+  home: ListViewProduct(),
+  title: 'Product DB',
+));
